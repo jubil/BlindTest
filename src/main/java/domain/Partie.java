167255b09e0406c57ea1efc3d863ba0b105cc2d6
Fournier <matthieu.fournier@etu.univ-nantes.fr>
2017-02-21 16:33:04 +0100

    Gestion des partie

--- src/main/java/domain/Partie.java
+++ src/main/java/domain/Partie.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Partie {
@@ -8,5 +9,13 @@ public class Partie {
 	private ArrayList<Chanson> chansons;
 	private int indexChansonCourante;
 	private HashMap<String, Integer> scores;
+	private static long startMusiqueTime;
+	//new Date().getTime();
+	
+	
+	public String getMusique(String pseudo) {
+		// TODO Auto-generated method stub
+		return "{Musique}";
+	}
 	
 }
---
+++ src/main/java/servlet/GestionPartie.java
@@ -0,0 +1,75 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domain.Partie;
+
+/**
+ * Servlet implementation class GestionPartie
+ */
+public class GestionPartie extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Partie partieCourante;
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GestionPartie() {
+        super();
+        partieCourante = new Partie();
+        
+       
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO changer le type pour json
+		response.setContentType("text/html");
+		
+		HttpSession session = request.getSession();
+		
+		// TODO : a supprimer une fois connexion operationnel
+		PrintWriter out = response.getWriter();
+		out.print("<br>Served at: " + request.getContextPath());
+		//session.setAttribute("pseudo", "Matthieu");
+		
+		String pseudo = (String) session.getAttribute("pseudo");
+		
+		if(pseudo == null){
+			pseudo = "Invité_" + new Date().getTime();
+			session.setAttribute("pseudo", pseudo);
+		}
+
+		// TODO : a supprimé quand op
+		out.print("<br>Pseudo : " + pseudo);
+		out.print("<br>Date : " + new Date().getTime());
+		
+		out.println("<br>" + partieCourante.getMusique(pseudo));
+		
+		
+		
+		
+		
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
