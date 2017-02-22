90675e367794b71fc949f26c75cb902bf0558026
jubil <valentin.53@orange.fr>
2017-02-22 15:48:23 +0100

    Fix de la creation d'utilisateur à partir du DataHandler

--- src/main/java/infra/DataHandler.java
+++ src/main/java/infra/DataHandler.java
@@ -12,28 +12,41 @@ public class DataHandler {
 	private static final String DATABASE_URL = "jdbc:sqlite:BDD.db";
 	private static Connection con = null;
 	
+	//OK
 	public void storeUser(User u){
+		if(con == null){
+			initDatabaseConnection();
+		}
+		
 		Statement statement;
 		try {
 			statement = con.createStatement();
 			statement.execute("INSERT INTO User (pseudo,password) VALUES ('"+u.getPseudo()+"','"+u.getPassword()+"'); SELECT rowid as ROWID, * FROM User;");
 		} catch (SQLException e) {
-			e.printStackTrace();
+			System.err.println(u.getPseudo() + " ne peut pas être ajouté à la base");
 		}
 		
 	}
 	
+	//OK
+	private void initDatabaseConnection() {
+		try {
+			con = DriverManager.getConnection(DATABASE_URL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	//OK
 	public static void createNewDatabase() {
 		if(con != null){
 			return;
 		}
-		String url = "jdbc:sqlite:BDD.db";
 		try {
-			con = DriverManager.getConnection(url);
+			con = DriverManager.getConnection(DATABASE_URL);
 			
 			Statement statement = con.createStatement();
 			statement.execute("CREATE TABLE IF NOT EXISTS User(pseudo VARCHAR PRIMARY KEY, password VARCHAR);");
-			
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
