13e20491a9a631d712bfeb9b829f0df64775f2d1
jubil <valentin.53@orange.fr>
2017-02-22 14:51:38 +0100

    Rename dataHandler.java to DataHandler.java

---
+++ src/main/java/infra/DataHandler.java
@@ -0,0 +1,53 @@
+package infra;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import domain.User;
+
+public class DataHandler {
+
+	private static Connection con;
+	
+	public void storeUser(User u){
+		
+	}
+	
+	public void showAllUsers() throws SQLException{
+		if(con == null){
+			getConnection();
+		}
+		
+		Statement state = con.createStatement();
+		ResultSet res = state.executeQuery("SELECT pseudo, password FROM user");
+		
+		//Affichage console
+		
+	}
+
+	private void getConnection() throws SQLException {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		con = DriverManager.getConnection("jdbc:sqlite:SQLiteTest.db");
+		initialise();
+	}
+
+	private void initialise() throws SQLException {
+		// TODO Auto-generated method stub
+		Statement state = con.createStatement();
+		ResultSet res = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='user'");
+	
+		if(!res.next()){
+			System.out.println("Initialisation de la BD");
+		}
+	}
+	
+	
+}
