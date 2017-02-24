53f8a36ff8e1d1fb9015ec830789789dba7890cd
jubil <valentin.53@orange.fr>
2017-02-24 14:21:34 +0100

    NewMusic devient AjouterMusique

--- src/main/java/servlet/AjouterMusique.java
+++ src/main/java/servlet/AjouterMusique.java
@@ -1,25 +1,33 @@
 package servlet;
 
+import infra.DataHandler;
+
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class NewMusic extends HttpServlet {
+public class AjouterMusique extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public NewMusic() {
+    public AjouterMusique() {
         super();
-        // TODO Auto-generated constructor stub
+        DataHandler.createNewDatabase();
+    	DataHandler.initDatabaseConnection();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doPost(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String titre = request.getParameter("titre");
+		String auteur = request.getParameter("auteur");
+		int categorie = Integer.valueOf(request.getParameter("cat"));
+		String musique = request.getParameter("musique");
+		String album = request.getParameter("album");
 	}
 
 }
