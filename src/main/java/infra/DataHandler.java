3d52ef2553560deea7112dcd7c3eba72a5decec9
jubil <valentin.53@orange.fr>
2017-02-24 14:12:22 +0100

    fix dataHandler

--- src/main/java/infra/DataHandler.java
+++ src/main/java/infra/DataHandler.java
@@ -26,7 +26,7 @@ public class DataHandler {
 		
 		try {
 			Statement statement = con.createStatement();
-			statement.execute("INSERT INTO Music (titre,auteur,categorie,fichier_musique,image_album) VALUES ('t','a','7','fm','fa'); SELECT rowid as ROWID, * FROM Music; VALUES ('"+c.getTitre()+"','"+c.getAuteur()+"','"+c.getSrcMusique()+"','"+c.getImgAlbum()+"'); SELECT rowid as ROWID, * FROM Music;");
+			statement.execute("INSERT INTO Music (titre,auteur,categorie,fichier_musique,image_album) VALUES ('t','a','"+c.getCategorie().getIdCategorie()+"','fm','fa'); SELECT rowid as ROWID, * FROM Music; VALUES ('"+c.getTitre()+"','"+c.getAuteur()+"','"+c.getSrcMusique()+"','"+c.getImgAlbum()+"'); SELECT rowid as ROWID, * FROM Music;");
 		} catch (SQLException e) {
 			System.err.println("La chanson ne peut pas être ajouté à la base");
 		}
